791dbc973ceab862d243fb0ab734022eb933e142
kris <61835777+KrisTheNewest@users.noreply.github.com>
2022-08-15 21:09:20 +0200

    handling delete

---
+++ validators/paramValidator.js
@@ -0,0 +1,11 @@
+const { param } = require('express-validator');
+exports.all = [
+	param("chara", "need a valid chara ID")
+	.isMongoId(),
+	param("costume", "need a valid costume ID")
+	.isMongoId(),
+	param("price", "need a valid price ID")
+	.isMongoId(),
+];
+
+// modules.exports = all;
