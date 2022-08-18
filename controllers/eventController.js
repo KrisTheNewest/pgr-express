0d56d4fe912625c6907baee6302bf8c4a429b850
kris <61835777+KrisTheNewest@users.noreply.github.com>
2022-08-19 01:03:11 +0200

    price ported to event

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var util = require("util")
 var createError = require('http-errors');
-const { body, validationResult } = require('express-validator');
+const { body, param, validationResult } = require('express-validator');
 const logger = require("../logger.js");
 	
 const Costume = require("../charasSchema.js");
@@ -29,9 +29,95 @@ exports.insert = [
 ];
 
 exports.update = [
+	(req, res, next) => {
+		// let filtered = Object.fromEntries(
+		req.body = req.body.costumes[0].event[0];
+		Object.entries(req.body).forEach(([key, item]) => {
+			if	(item.length === 0) {
+				delete req.body[key];
+			}
+		})
+		next();
+	},	
 
+	param("chara", "need a valid chara ID")
+		.isMongoId(),
+	param("costume", "need a valid costume ID")
+		.isMongoId(),
+	param("price", "need a valid event ID")
+		.isMongoId(),
+
+	// TODO: NEED TO VALIDATE
+	// TODO: USE/MAKE A REUSABLE FILE 
+	
+	async (req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			console.log("errors", errors.array());
+			return next(createError(500, errors.array()));
+		}
+		
+		let oldData = await findSubDoc(Costume, req.params);//.then(data => data).catch(err => next(createError(err.status || 500, err)))
+		// console.log(oldData);
+		if (oldData instanceof Error) return next(oldData);
+		// res.redirect("back");
+		// return;
+		// console.log(oldData);
+		let [character, oldPrice] = oldData;
+		// console.log(character)
+		// console.log(oldPrice)
+		let newPrice = {...oldPrice, ...req.body, _id: req.params.price};
+		// console.log(newPrice)
+		await character.updateOne(
+			{ "$set": { "costumes.$[costId].price.$[priceId]": newPrice}},
+			{ "arrayFilters": 
+				[{"costId._id": req.params.costume }, {"priceId._id": req.params.price}]
+			},
+			
+			(err, changes) => { 
+				if (err) throw err;
+				//console.log(changes)
+				res.redirect(`/costumes/${character.frameName}/${req.params.costume}`);
+			}
+		);
+	},
+	// (req, res, next) => {
+	// 	res.redirect("back");
+	// },
 ];
 
 exports.delete = [
-	
+	param("chara", "need a valid chara ID")
+		.isMongoId(),
+	param("costume", "need a valid costume ID")
+		.isMongoId(),
+	param("event", "need a valid event ID")
+		.isMongoId(),
+	async (req, res, next) => {
+		// let filtered = Object.fromEntries(
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			console.log("errors", errors.array());
+			return next(createError(500, errors.array()));
+		}
+		let oldData = await findSubDoc(Costume, req.params);//.then(data => data).catch(err => next(createError(err.status || 500, err)))
+		// console.log(oldData);
+		if (oldData instanceof Error) return next(oldData);
+		// res.redirect("back");
+		// return;
+		// console.log(oldData);
+		let [character] = oldData;
+		await character.updateOne(
+			// {"charaName": "Luna"}, 
+			{ "$pull":  {"costumes.$[costId].event": {_id: req.params.event}}},
+			{ "arrayFilters": 
+				[{"costId._id": req.params.costume}]
+			},
+			(err, changes) => { 
+				if (err) throw err;
+				//console.log(changes)
+				res.redirect(`/costumes/${character.frameName}/${req.params.costume}`);
+			}
+		);
+	},	
 ];
