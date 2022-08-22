67984415e4acd821014abc9b7c08956aca1416b5
kris <61835777+KrisTheNewest@users.noreply.github.com>
2022-08-22 15:05:46 +0200

    svelte test

---
+++ test.svelte
@@ -0,0 +1,25 @@
+<script>
+	let selected;
+	
+	const obj = {
+		a: 123,
+		b: "abc"
+	}
+	
+	function selectChara() {
+		console.log(selected);
+	}
+</script>
+
+<h1>Select</h1>
+<form on:submit|preventDefault={selectChara}>
+	<h3>Character</h3>
+	<select bind:value={selected}>
+		<option value="123">test1</option>
+		<option value="987">test2</option>
+		<option value="xyz">test3</option>
+	</select>
+		<button type=submit>Submit</button>
+</form>
+
+<p>selected question {selected ? selected : '[waiting...]'}</p>
