2b28850ec62815a61a0d0fbff221782ea7f26e46
KrisTheNewest <61835777+KrisTheNewest@users.noreply.github.com>
2024-11-14 20:55:58 +0100

    long overdue small index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, ) => {
-  res.redirect(301, "./costumes")
+// Redirecting to the costumes route (leaving out the index route for future)
+router.get('/', (req, res) => {
+  res.redirect(301, "./costumes");
 });
 
 module.exports = router;
